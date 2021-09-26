228c14d2533bfd2223241a94d4f09299080ff745
lodthomaz <leticiaoterodiasthomaz@gmail.com>
2021-09-26 17:21:25 -0300

    Códigos dos modelos finais

--- exemplo1_iris_dataset.R
+++ exemplo1_iris_dataset.R
@@ -6,8 +6,6 @@ library(nnet)
 # Base de Dados: Exemplo 1
 df_iris <- iris
 
-head(iris)
-
 # Variável Resposta
 df_iris %>% 
   group_by(Species) %>% 
@@ -17,18 +15,27 @@ df_iris %>%
 aux <- createDataPartition(iris$Species, p = .80, list = FALSE)
 
 df_treino <- df_iris[aux,]
-df_test <- df_iris[-aux,]
-
+df_teste <- df_iris[-aux,]
 
 # Escolha da Categoria de Referência
 df_treino$Species <- relevel(df_treino$Species, ref = "virginica")
+df_teste$Species <- relevel(df_teste$Species, ref = "virginica")
 
 # Treino do Modelo
 modelo <- multinom(Species ~ ., data = df_treino)
 
-# Checking the model
+# Resumo do Modelo
 summary(modelo)
 
-exp(coef(modelo))
+# Valores preditos da base de treino
+df_treino$fitted_values <- predict(modelo, newdata = df_treino, "class")
 
-round(fitted(modelo), 2)
+# Matriz de Confusão
+confusionMatrix(df_treino$Species, df_treino$fitted_values)
+
+
+# Valores preditos da base de teste
+df_teste$fitted_values <- predict(modelo, newdata = df_teste, "class")
+
+# Matriz de Confusão
+confusionMatrix(df_teste$Species, df_teste$fitted_values)
--- exemplo2_titanic.R
+++ exemplo2_titanic.R
@@ -1,4 +1,57 @@
-
-
+# Pacotes
+library(dplyr)
+library(caret)
+library(nnet)
+library(skimr)
 
 # Dados (https://www.kaggle.com/c/titanic/data)
+df_titanic <- read.csv2("Data/train.csv", sep = ',', dec = '.') %>% 
+  mutate(Pclass = as.factor(Pclass)) %>% 
+  select(Pclass, Survived, Sex, Age, SibSp, Parch, Embarked)
+
+# Variável Resposta
+df_titanic %>% 
+  group_by(Pclass) %>% 
+  count()
+
+# skim
+skim(df_titanic)
+
+# Tratamento da base de dados 
+df_titanic_trat <- df_titanic %>% 
+  filter(Embarked != "") %>% 
+  mutate(Age = coalesce(Age, median(df_titanic$Age, na.rm = TRUE)))
+
+# skim
+skim(df_titanic_trat)
+
+# Separação da Base em Treino e Teste
+aux <- createDataPartition(df_titanic_trat$Pclass, p = .80, list = FALSE)
+
+df_treino <- df_titanic_trat[aux,]
+df_teste <- df_titanic_trat[-aux,]
+
+# Escolha da Categoria de Referência
+df_treino$Pclass <- relevel(df_treino$Pclass, ref = "1")
+
+# Treino do Modelo
+modelo <- multinom(Pclass ~ ., data = df_treino)
+
+# Resumo do Modelo
+summary(modelo)
+
+# Exponencial dos Coeficientes do Modelo
+exp(coef(modelo))
+
+# Valores preditos da base de treino
+df_treino$fitted_values <- predict(modelo, newdata = df_treino, "class")
+
+# Matriz de Confusão
+confusionMatrix(df_treino$Pclass, df_treino$fitted_values)
+
+# Valores preditos da base de teste
+df_teste$fitted_values <- predict(modelo, newdata = df_teste, "class")
+
+# Matriz de Confusão
+confusionMatrix(df_teste$Pclass, df_teste$fitted_values)
+
